3ffc0feaccb7557d87892b750b43fd12406b5116
negaonka <neha.n.gaonkar@gmail.com>
2022-10-26 02:51:52 +0100

    changing few routes

--- controllers/api/UserRoute.js
+++ controllers/api/UserRoute.js
@@ -1,85 +1,89 @@
-const router = require('express').Router();
-const { User } = require('../../models');
+const router = require("express").Router();
+const { User } = require("../../models");
 
 /* -------------------------- render logged in page ------------------------- */
-router.get('/login', async (req, res) => {
-    try {
-        res.render('login');
-    } catch (err) {
-        res.status(500).json(err);
-    }
+router.get("/login", async (req, res) => {
+  try {
+    res.render("login");
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 /* --------------------------- render sign up page -------------------------- */
 
-router.get('/signup', async (req, res) => {
-    try {
-        res.render('signup');
-    } catch (err) {
-        res.status(400).json(err);
-    }
+router.get("/signup", async (req, res) => {
+  try {
+    res.render("signup");
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
 /* ---------------------------- Create a new user --------------------------- */
 
-router.post('/', async (req, res) => {
-    try {
-        const userData = await User.create(req.body);
+router.post("/", async (req, res) => {
+  try {
+    const userData = await User.create(req.body);
 
-        req.session.save(() => {
-            req.session.user_id = userData.id;
-            req.session.logged_in = true;
+    req.session.save(() => {
+      req.session.user_id = userData.id;
+      req.session.logged_in = true;
 
-            res.status(200).json(userData);
-        });
-    } catch (err) {
-        res.status(400).json(err);
-    }
+      res.status(200).json(userData);
+    });
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
 /* ---------------------------- User login route ---------------------------- */
 
-router.post('/login', async (req, res) => {
-    try {
-        // Find the user who matches the posted e-mail address
-        const userData = await User.findOne({ where: { email: req.body.email } });
+router.post("/login", async (req, res) => {
+  try {
+    // Find the user who matches the posted e-mail address
+    const userData = await User.findOne({ where: { email: req.body.email } });
 
-        if (!userData) {
-            res.status(400).json({ message: 'Incorrect email or password, please try again' });
-            return;
-        }
-
-        // Verify the posted password with the password store in the database
-        const validPassword = await userData.checkPassword(req.body.password);
-
-        if (!validPassword) {
-            res.status(400).json({ message: 'Incorrect email or password, please try again' });
-            return;
-        }
-
-        // Create session variables based on the logged in user
-        req.session.save(() => {
-            req.session.user_id = userData.id;
-            req.session.logged_in = true;
-
-            res.status(200).json({ user: userData, message: 'You are now logged in!' });
-        });
-    } catch (err) {
-        res.status(400).json(err);
+    if (!userData) {
+      res
+        .status(400)
+        .json({ message: "Incorrect email or password, please try again" });
+      return;
     }
+
+    // Verify the posted password with the password store in the database
+    const validPassword = await userData.checkPassword(req.body.password);
+
+    if (!validPassword) {
+      res
+        .status(400)
+        .json({ message: "Incorrect email or password, please try again" });
+      return;
+    }
+
+    // Create session variables based on the logged in user
+    req.session.save(() => {
+      req.session.user_id = userData.id;
+      req.session.logged_in = true;
+
+      res.json({ user: userData, message: "You are now logged in!" });
+    });
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
 /* ---------------------------- user logout route --------------------------- */
 
-router.post('/logout', (req, res) => {
-    if (req.session.logged_in) {
-        // Remove the session variables
-        req.session.destroy(() => {
-            res.status(204).json({ message: 'You are logged out!' }).end();
-        });
-    } else {
-        res.status(404).end();
-    }
+router.post("/logout", (req, res) => {
+  if (req.session.logged_in) {
+    // Remove the session variables
+    req.session.destroy(() => {
+      res.status(204).json({ message: "You are logged out!" }).end();
+    });
+  } else {
+    res.status(404).end();
+  }
 });
 
 module.exports = router;
--- controllers/productPageRoutes.js
+++ controllers/productPageRoutes.js
@@ -1,48 +1,55 @@
-const router = require('express').Router();
-const { Op } = require('sequelize');
-const { User, Product, Category, Tag } = require('../models');
+const router = require("express").Router();
+const { Op } = require("sequelize");
+const { User, Product, Category, Tag } = require("../models");
 
 // GET a product by it's 'id' value
-router.get('/:id', async (req, res) => {
-    // find a single product by its `id`
-    try {
-        const productData = await Product.findByPk(req.params.id, { include: ({ model: Category }, { model: Tag }) });
-        if (!productData) {
-            res.status(404).json({ message: 'No product found with this id!' });
-            return;
-        }
-        res.render('product-page', {
-            productData,
-        });
-    } catch (err) {
-        res.status(500).json(err);
+router.get("/:id", async (req, res) => {
+  // find a single product by its `id`
+  try {
+    const productData = await Product.findByPk(req.params.id, {
+      include: ({ model: Category }, { model: Tag }),
+    });
+
+    if (!productData) {
+      res.status(404).json({ message: "No product found with this id!" });
+      return;
     }
+    const productItem = productData.get({ plain: true });
+    console.log(productItem);
+    res.render("product-page", {
+      productItem,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.get('/:name', async (req, res) => {
-    try {
-        console.log(req.params.name);
-        const productData = await Product.findAll({
-            where: {
-                name: {
-                    [Op.substring]: req.params.name,
-                },
-            },
-        });
+router.get("/:name", async (req, res) => {
+  try {
+    console.log(req.params.name);
+    const productData = await Product.findAll({
+      where: {
+        name: {
+          [Op.substring]: req.params.name,
+        },
+      },
+    });
 
-        if (!productData) {
-            res.status(404).json({ message: 'No products with this id!' });
-            return;
-        }
-
-        const products = productData.map((productItem) => productItem.get({ plain: true }));
-        console.log(products);
-        res.render('productpage', {
-            products,
-        });
-    } catch (err) {
-        res.status(400).json(err);
+    if (!productData) {
+      res.status(404).json({ message: "No products with this id!" });
+      return;
     }
+
+    const products = productData.map((productItem) =>
+      productItem.get({ plain: true })
+    );
+    console.log(products);
+    res.render("productpage", {
+      products,
+    });
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
 module.exports = router;
